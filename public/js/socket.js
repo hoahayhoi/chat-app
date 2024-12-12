d0b6c93f93a9403fe38cec5d640d8944d11b0823
hoahayhoi <hoa.tmh2003@gmail.com>
2024-12-12 17:14:33 +0700

    basic chat without realtime

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,2 +1,18 @@
 var socket = io();
-console.log("client");
+
+// CLIENT_SEND_MESSAGE
+const formChat = document.querySelector(".chat .inner-form");
+if(formChat) {
+  formChat.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const content = formChat.content.value;
+    if(content) {
+      const data = {
+        content: content
+      };
+      socket.emit("CLIENT_SEND_MESSAGE", data);
+      formChat.content.value = "";
+    }
+  })
+}
+// End CLIENT_SEND_MESSAGE
