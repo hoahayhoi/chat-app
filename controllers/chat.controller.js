535483b4cc492910633e0b3cbe0d28f3a30c9870
hoahayhoi <hoa.tmh2003@gmail.com>
2024-12-19 10:45:34 +0700

    private roomChat

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -5,20 +5,23 @@ const streamUploadHelper = require("../helpers/streamUpload.helper");
 
 module.exports.index = async (req, res) => {
     _io.once("connection", (socket) => {
+        // For private room 
+        socket.join(req.params.roomChatId);
+
         // Người dùng gửi tin nhắn lên server
         socket.on("CLIENT_SEND_MESSAGE", async (data) => {
             const images = [];
 
             for (const item of data.images) {
-              const result = await streamUploadHelper.streamUpload(item);
-              images.push(result.url);
+                const result = await streamUploadHelper.streamUpload(item);
+                images.push(result.url);
             }
 
             const dataChat = {
-              userId: res.locals.user.id,
-              // roomChatId: String,
-              content: data.content,
-              images: images,
+                userId: res.locals.user.id,
+                roomChatId: req.params.roomChatId,
+                content: data.content,
+                images: images,
             };
 
             // Lưu tin nhắn vào database
@@ -26,17 +29,17 @@ module.exports.index = async (req, res) => {
             await chat.save();
 
             // Trả data về cho client
-            _io.emit("SERVER_RETURN_MESSAGE", {
-              userId: res.locals.user.id,
-              fullName: res.locals.user.fullName,
-              content: data.content,
-              images: images
+            _io.to(req.params.roomChatId).emit("SERVER_RETURN_MESSAGE", {
+                userId: res.locals.user.id,
+                fullName: res.locals.user.fullName,
+                content: data.content,
+                images: images
             })
         })
 
         // CLIENT_SEND_TYPING
         socket.on("CLIENT_SEND_TYPING", (type) => {
-            socket.broadcast.emit("SERVER_RETURN_TYPING", {
+            socket.broadcast.to(req.params.roomChatId).emit("SERVER_RETURN_TYPING", {
                 userId: res.locals.user.id,
                 fullName: res.locals.user.fullName,
                 type: type
@@ -46,6 +49,7 @@ module.exports.index = async (req, res) => {
 
     // Lấy tin nhắn mặc định
     const chats = await Chat.find({
+        roomChatId: req.params.roomChatId,         
         deleted: false
     });
 
